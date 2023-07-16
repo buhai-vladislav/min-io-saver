8972cea21bef7f5906c99a9ae5c73e305f0490f8
Vladyslav Buhai <vladbugaj30@gmail.com>
2023-07-16 12:12:02 +0300

    add toast container component and styles for it

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -5,6 +5,8 @@ import App from './App';
 import { NextUIProvider } from '@nextui-org/react';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -16,5 +18,6 @@ root.render(
         <App />
       </React.StrictMode>
     </NextUIProvider>
+    <ToastContainer />
   </Provider>,
 );
